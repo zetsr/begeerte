5d7f76a146427fed4000c12dfb46c63e9213219d
zetsr <90092893+zetsr@users.noreply.github.com>
2024-12-15 21:05:05 +0800

    Add files via upload

---
+++ begeerte/Cheat/RunMM.cpp
@@ -0,0 +1,83 @@
+#include "..\pch.h" 
+#include "RunMM.h"
+#include "Utils.h"
+#include "MM.h" 
+#include <string>    
+
+// 修改 FrameGen 的函数
+void HookFrameGen(HANDLE hProcess, uintptr_t moduleBaseAddress) {
+    /*
+    2024/11/16
+    : 适配最新版本Version 1.1.7
+
+    2024/11/7
+    : 适配最新版本Version 1.1.4
+
+    2024/11/3
+    : 适配最新版本Version 1.1.3
+
+    Version 1.1.2 实际上并未影响帧生成的内存地址和偏移。
+    不知道为什么他们仍未修复这个问题，也许他们并不认为这是一个“错误”，因为除此之外我想不通其他能够对此解释的理由。
+    也许应该转向sig作为扫描特征而不是使用基址+偏移的形式，因为后者几乎每次更新都需要重新逆向。
+    此外，我仍然无法正确钩住d3d12，这导致几乎不可能注入imgui进行实时调试。
+    
+    这个项目简直糟透了...这个游戏也糟透了。
+    */
+
+    const uintptr_t firstOffset = 0x082E1CE0; 
+    const uintptr_t finalOffset = 0x4;
+
+    HookMemory frameGen(hProcess, moduleBaseAddress, firstOffset, finalOffset);
+
+    int expectedValue = 1; // 1 = On，0 = Off
+    int currentValue = frameGen.GetInt();
+
+    if (currentValue != expectedValue) {
+        print(("Try set FrameGen to " + std::to_string(expectedValue) +
+            " at Offset: " + std::to_string(firstOffset) +
+            "+" + std::to_string(finalOffset)).c_str());
+        frameGen.SetInt(expectedValue);
+    }
+}
+
+// 修改渲染分辨率
+void HookRenderResolution(HANDLE hProcess, uintptr_t moduleBaseAddress) {
+    /*
+    没有意义
+    Quality和Ultra Quality其实区别不大
+    */
+    const uintptr_t firstOffset = 0x085A3720;
+    const uintptr_t finalOffset = 0x0;
+
+    HookMemory RenderResolution(hProcess, moduleBaseAddress, firstOffset, finalOffset);
+
+    float expectedValue = 75.f; // 75.f = Ultra Quality
+    float currentValue = RenderResolution.GetFloat();
+
+    if (currentValue != expectedValue) {
+        print(("Try set RenderResolution to " + std::to_string(expectedValue) +
+            " at Offset: " + std::to_string(firstOffset) +
+            "+" + std::to_string(finalOffset)).c_str());
+        RenderResolution.SetFloat(expectedValue);
+    }
+}
+
+// 修改内存值的函数
+void modifyMemoryValue() {
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
+    if (hProcess == NULL) {
+        print("Failed to open process.");
+        return;
+    }
+
+    try {
+        uintptr_t moduleBaseAddress = (uintptr_t)GetModuleHandle(L"Dragons-Win64-Shipping.exe");
+        HookFrameGen(hProcess, moduleBaseAddress);
+        HookRenderResolution(hProcess, moduleBaseAddress);
+    }
+    catch (...) {
+        print("An exception occurred by modifyMemoryValue.");
+    }
+
+    CloseHandle(hProcess);
+}
