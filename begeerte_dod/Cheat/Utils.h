cb1928bf883a4ff1595492a1658a50ecf04c6a5c
zetsr <90092893+zetsr@users.noreply.github.com>
2025-01-14 03:20:50 +0800

    Add files via upload

---
+++ begeerte_dod/Cheat/Utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+std::string tcharToString(const TCHAR* tcharStr);
+void printProcessInfo();
+void printSystemInfo();
+
+#endif // UTILS_H
+#pragma once
